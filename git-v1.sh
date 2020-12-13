296d90b444a786114601dc5e958744bd15f6b606
TheCynicalLiger <wayne6324@gmail.com>
2020-12-13 18:40:08 +0000

    Update git scripts

--- git-v1.sh
+++ git-v1.sh
@@ -28,7 +28,7 @@ git commit -m "$input"
 
 # Push the local files to github
 
-git push -u origin main
+git push -u origin Master
 
 
 echo "################################################################"
