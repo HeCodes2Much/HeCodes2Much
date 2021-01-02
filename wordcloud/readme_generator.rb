97e4aaaa03e134761eb7b109549f7c13257cf832
TheCynicalLiger <wayne6324@gmail.com>
2021-01-02 17:37:43 +0000

    2021

--- wordcloud/readme_generator.rb
+++ wordcloud/readme_generator.rb
@@ -42,7 +42,7 @@ class ReadmeGenerator
 
       - 🌱 I’m currently learning everything 🤣
       - 👯 I’m looking to collaborate with other content creators
-      - 🥅 2020 Goals: Contribute more to Open Source projects
+      - 🥅 2021 Goals: Contribute more to Open Source projects
       - ⚡ Fun fact: I love nespresso coffee
 
       ---
