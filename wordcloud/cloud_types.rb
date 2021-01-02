7e18c0e7f19d1094021da76126068458cdd80c3d
TheCynicalLiger <wayne6324@gmail.com>
2021-01-02 16:56:30 +0000

    test

--- wordcloud/cloud_types.rb
+++ wordcloud/cloud_types.rb
@@ -2,14 +2,12 @@ module CloudTypes
   CLOUDLABELS = [
     "quarantine",
     "food",
-    "company",
     "github",
   ]
 
   CLOUDPROMPTS = [
     "Favourite Quarantine Passtime? :lock: :tennis: :video_game:",
     "Favourite Food? :yum: :pizza: :cookie:",
-    "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:",
     "Favourite Github Repo? :lock: :cop: :blue_heart:",
   ]
 end
